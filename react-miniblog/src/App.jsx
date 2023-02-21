4e227ffbf701fe23215495121b501e1f52964b46
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-20 22:40:22 -0300

    Adicionando Navbar e footer

--- react-miniblog/src/App.jsx
+++ react-miniblog/src/App.jsx
@@ -4,17 +4,22 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 // pages
 import { Home, About } from './page'
 
+// componentes
+import { Navbar, Footer } from './components'
+
 function App() {
 
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="container">
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </div>
   )
