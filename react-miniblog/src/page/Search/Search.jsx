1d084b7f06b6f76e6c176e98c56be1b9e35bb825
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-25 13:19:46 -0300

    Exibindo resultados da busca

--- react-miniblog/src/page/Search/Search.jsx
+++ react-miniblog/src/page/Search/Search.jsx
@@ -1,16 +1,35 @@
 import styles from './Search.module.css'
 
+import { Link } from 'react-router-dom'
+
 // hooks
 import { useFetchDocuments, useQuery } from '../../hooks'
+
+// componentes
+import { PostDetail } from '../../components'
 
 const Search = () => {
   const query = useQuery()
   const search = query.get('q')
 
+  const { documents: posts } = useFetchDocuments('posts', search)
+
   return (
-    <div>
+    <div className={styles.search_container}>
       <h1>Search</h1>
-      <p>{search}</p>
+      <div>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Nenhum resultado encontrado a partir da busca <span>"{search}"</span>.</p>
+            <Link to='/' className='btn btn-dark'>
+              Voltar
+            </Link>
+          </div>
+        )}
+        {posts && posts.map((post) => (
+          <PostDetail key={post.id} post={post} />
+        ))}
+      </div>
     </div>
   )
 }
