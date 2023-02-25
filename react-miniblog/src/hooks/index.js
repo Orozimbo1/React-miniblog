84fb53a4bc390012b557db7ac4741680f3b3ca8b
Matheus Orozimbo <matheusorozimbo5@gmail.com>
2023-02-24 21:05:14 -0300

    Hook de resgate de dados

--- react-miniblog/src/hooks/index.js
+++ react-miniblog/src/hooks/index.js
@@ -1,2 +1,3 @@
 export { default as useAuthentication } from './useAuthentication'
 export { default as useInsertDocument } from './useInsertDocument'
+export { default as useFetchDocuments } from './useFetchDocuments'
